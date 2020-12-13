c91ee55ff36088313f269b39af2affce467f0487
FMariaD <fedorova.md@phystech.edu>
2020-12-13 06:03:48 +0300

    новый класс стен, изменение вида платформ

--- blocks.py
+++ blocks.py
@@ -3,14 +3,24 @@ from pygame import *
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
 COLOR1 = (200, 180, 0)
+COLOR2 = (255, 135, 0)
 COLOR3 = (0, 0, 255)
+
+
+class Wall(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.size = (PLATFORM_WIDTH, PLATFORM_HEIGHT)
+        self.image = Surface((self.size[0], self.size[1]))
+        self.image.fill(Color(COLOR2))
+        self.rect = self.image.get_rect(topleft=(x, y))
 
 
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.size = (PLATFORM_WIDTH, PLATFORM_HEIGHT)
-        self.image = Surface((self.size[0], self.size[1]))
+        self.image = Surface((self.size[0], self.size[1] // 2))
         self.image.fill(Color(COLOR1))
         self.rect = self.image.get_rect(topleft=(x, y))
 
@@ -31,6 +41,7 @@ class Spike(Platform):
 class Teleport(Platform):
     def __init__(self, x, y, goX, goY):
         Platform.__init__(self, x, y)
+        self.image = Surface((self.size[0], self.size[1]))
         self.goX = goX  # координаты назначения перемещения
         self.goY = goY  # координаты назначения перемещения
         self.image.fill(COLOR3)
