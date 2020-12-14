79392e98a34babdef8e2cc2817a4f71a53134634
AALych <71018792+AALych@users.noreply.github.com>
2020-12-14 17:05:12 +0300

    Теперь лучше отрисовывается

--- blocks.py
+++ blocks.py
@@ -42,6 +42,6 @@ class Teleport(Platform):
     def __init__(self, x, y, goX, goY):
         Platform.__init__(self, x, y)
         self.image = image.load("portal.png")
-        self.rect = self.image.get_rect(self.image, center=(x, y))
+        self.rect = self.image.get_rect(topleft=(x, y))
         self.goX = goX  # координаты назначения перемещения
         self.goY = goY  # координаты назначения перемещения
